d5b8d565c89f151d51f81facff59001510c3c863
Kevin520122 <64236884+Kevin520122@users.noreply.github.com>
2022-09-13 13:14:33 -0700

    3 files modified

--- mvc/view.js
+++ mvc/view.js
@@ -76,14 +76,18 @@ const View = (() => {
 			
 			tmp += `
         <li>
-          <span>${todo.title}</span>
-          <button class="deletebtn" id="${todo.id}">X</button>
-          <div class="edit"> 
-            <i class="fa fa-edit" id="${todo.id}"></i>
-          </div>
-          
-          	<svg  class="finish1" id="${todo.id}" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowForwardIcon" aria-label="fontSize small"><path class="finish2" d="m12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></svg>
-					
+					<div class="text-area">
+          	<span>${todo.title}</span>
+					</div>
+					<div class="button-area">
+         		 <svg class="editbtn" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path id="delete" d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>
+
+							<svg class="deletebtn" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path id="edit" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
+
+							<svg class="finish" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowForwardIcon" aria-label="fontSize small"><path id="done"d="m12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></svg>
+					</div>
+
+
             
         </li>
       `;
