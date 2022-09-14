3c0112714a051c2ceba78b6f3e9a919b920d0cea
Kevin520122 <64236884+Kevin520122@users.noreply.github.com>
2022-09-14 11:13:08 -0700

    4 files modified

--- mvc/controller.js
+++ mvc/controller.js
@@ -4,40 +4,39 @@ import View from './view.js';
 // * ~~~~~~~~~~~~~~~~~~~ Controller ~~~~~~~~~~~~~~~~~~~
 const Controller = ((model, view) => {
   const state = new model.State();
-  const getTodo = (id) => {
-    const state = new Model.State();
-    let res = '';
-    state.todolist.forEach((todo) => {
-      if (todo.id === id) {
-        res = todo.title;
-      }
-    });
-    return res;
-  };
-
   const deleteTodo = () => {
     const todocontainer = document.querySelector(view.domstr.todocontainer);
-    todocontainer.addEventListener('click', (event) => {
-      if (event.target.className === 'deletebtn') {
-        state.todolist = state.todolist.filter(
-          (todo) => +todo.id !== +event.target.id
-        );
-        model.deleteTodo(event.target.id);
-      }
+    const deletebtn = document.querySelector('#delete');
+
+    deletebtn.addEventListener('click', (event) => {
+      state.todolist = state.todolist.filter(
+        (todo) => +todo.id !== +event.target.id
+      );
+      model.deleteTodo(event.target.id);
     });
+    // todocontainer.addEventListener('click', (event) => {
+    //   if (event.target.id === 'delete') {
+    //     state.todolist = state.todolist.filter(
+    //       (todo) => +todo.id !== +event.target.id
+    //     );
+    //     model.deleteTodo(event.target.id);
+    //   }
+    // });
   };
 
   const deleteDone = () => {
-    const completed_container = document.querySelector(view.domstr.completed_container);
-    completed_container.addEventListener("click", (event) => {
-      if (event.target.className === 'deletebtn') {
+    const completed_container = document.querySelector(
+      view.domstr.completed_container
+    );
+    completed_container.addEventListener('click', (event) => {
+      if (event.target.id === 'delete') {
         state.completedlist = state.completedlist.filter(
           (todo) => +todo.id !== +event.target.id
         );
         model.deleteDone(event.target.id);
       }
-    })
-  }
+    });
+  };
 
   const addTodo = () => {
     const inputbox = document.querySelector(view.domstr.inputbox);
@@ -65,81 +64,113 @@ const Controller = ((model, view) => {
 
   const editTodo = () => {
     const todocontainer = document.querySelector(view.domstr.todocontainer);
-   
-    todocontainer.addEventListener('click', (event) => {
-      if (event.target.className === 'fa fa-edit') {
-        const cur = state.todolist.find((todo) => +todo.id === +event.target.id)
-        const index = state.todolist.indexOf(cur)
-        const tmp = view.editTmp(state.todolist, event.target.id)
-        view.render(todocontainer, tmp)
-        const edit_done = document.getElementById("submit_edit")
-        edit_done.addEventListener("click", (event) => {
-          const editbox = document.querySelector("#editbox");
-          cur.title = editbox.value
-          state.todolist[index] = cur
-          const tmp = view.editTmp(state.todolist, event.target.id)
-          view.render(todocontainer, tmp)
-          model.editTodo(cur.id, cur)
-        })     
-      }
-    });
+    const editbtn = document.querySelector('#edit');
+    editbtn.addEventListener('click', (event) => {
+      console.log(event.target.id);
 
-    
+      //Locate current todo
+      const cur = state.todolist.find((todo) => +todo.id === +event.target.id);
+      //Locate current todo index
+      const index = state.todolist.indexOf(cur);
+      //re-render the page
+      const tmp = view.editTmp(state.todolist, event.target.id);
+      view.render(todocontainer, tmp);
+
+      const edit_done = document.getElementById('submit_edit');
+      edit_done.addEventListener('click', (event) => {
+        const editbox = document.querySelector('#editbox');
+        cur.title = editbox.value;
+        state.todolist[index] = cur;
+        const tmp = view.editTmp(state.todolist, event.target.id);
+        view.render(todocontainer, tmp);
+        model.editTodo(cur.id, cur);
+      });
+    });
+    // todocontainer.addEventListener('click', (event) => {
+    //   console.log(event.target.id)
+    //   if (event.target.id ==='edit') {
+    //     //Locate current todo
+    //     const cur = state.todolist.find((todo) => +todo.id === +event.target.id)
+    //     //Locate current todo index
+    //     const index = state.todolist.indexOf(cur)
+    //     //re-render the page
+    //     const tmp = view.editTmp(state.todolist, event.target.id)
+    //     view.render(todocontainer, tmp)
+
+    //     const edit_done = document.getElementById("submit_edit")
+    //     edit_done.addEventListener("click", (event) => {
+    //       const editbox = document.querySelector("#editbox");
+    //       cur.title = editbox.value
+    //       state.todolist[index] = cur
+    //       const tmp = view.editTmp(state.todolist, event.target.id)
+    //       view.render(todocontainer, tmp)
+    //       model.editTodo(cur.id, cur)
+    //     })
+    //   }
+    // });
   };
 
   const editDone = () => {
-    const completed_container = document.querySelector(view.domstr.completed_container);
-   
+    const completed_container = document.querySelector(
+      view.domstr.completed_container
+    );
+
     completed_container.addEventListener('click', (event) => {
-      if (event.target.className === 'fa fa-edit') {
-        const cur = state.completedlist.find((todo) => +todo.id === +event.target.id)
-        const index = state.completedlist.indexOf(cur)
-        const tmp = view.editTmp2(state.completedlist, event.target.id)
-        view.render(completed_container, tmp)
-        const edit_done = document.getElementById("submit_edit")
-        edit_done.addEventListener("click", (event) => {
-          const editbox = document.querySelector("#editbox");
-          cur.title = editbox.value
-          state.completedlist[index] = cur
-          const tmp = view.editTmp2(state.completedlist, event.target.id)
-          view.render(completed_container, tmp)
-          model.editDone(cur.id, cur)
-        })     
+      if (event.target.id === 'edit') {
+        const cur = state.completedlist.find(
+          (todo) => +todo.id === +event.target.id
+        );
+        const index = state.completedlist.indexOf(cur);
+        const tmp = view.editTmp2(state.completedlist, event.target.id);
+        view.render(completed_container, tmp);
+        const edit_done = document.getElementById('submit_edit');
+        edit_done.addEventListener('click', (event) => {
+          const editbox = document.querySelector('#editbox');
+          cur.title = editbox.value;
+          state.completedlist[index] = cur;
+          const tmp = view.editTmp2(state.completedlist, event.target.id);
+          view.render(completed_container, tmp);
+          model.editDone(cur.id, cur);
+        });
       }
     });
-  }
+  };
 
   const completeTodo = async () => {
     const todocontainer = document.querySelector(view.domstr.todocontainer);
     todocontainer.addEventListener('click', async (event) => {
-      if (event.target.tagName === 'svg') {
-        console.log('finish');
-        const cur = state.todolist.find((todo) => +todo.id === +event.target.id)
-        console.log(cur)
+      if (event.target.id === 'done') {
+        console.log(event.target);
+        const cur = state.todolist.find(
+          (todo) => +todo.id === +event.target.id
+        );
+        console.log(cur);
         //Remove todo from pending list
         state.todolist = state.todolist.filter(
           (todo) => +todo.id !== +event.target.id
         );
         await model.deleteTodo(event.target.id);
         //Add todo in completed list
-        
+
         const todo = new model.Todo(cur.title);
-        todo.completed = true
+        todo.completed = true;
         await model.addDone(todo).then((todofromBE) => {
           state.completedlist = [todofromBE, ...state.completedlist];
         });
-       
       }
     });
   };
 
   const undo = async () => {
-    const completed_container = document.querySelector(view.domstr.completed_container);
+    const completed_container = document.querySelector(
+      view.domstr.completed_container
+    );
     completed_container.addEventListener('click', async (event) => {
-      if (event.target.tagName === 'svg') {
-       
-        const cur = state.completedlist.find((todo) => +todo.id === +event.target.id)
-        console.log(cur)
+      if (event.target.id === 'undo') {
+        const cur = state.completedlist.find(
+          (todo) => +todo.id === +event.target.id
+        );
+        console.log(cur);
         //Remove todo from completed list
         state.completedlist = state.completedlist.filter(
           (todo) => +todo.id !== +event.target.id
@@ -147,33 +178,31 @@ const Controller = ((model, view) => {
         await model.deleteDone(event.target.id);
         //Add todo in pending list
         const todo = new model.Todo(cur.title);
-        todo.completed = false
+        todo.completed = false;
         await model.addTodo(todo).then((todofromBE) => {
           state.todolist = [todofromBE, ...state.todolist];
         });
-       
       }
     });
-  }
+  };
 
-  const init =  () => {
-     model.getTodos().then((todos) => {
+  const init = () => {
+    model.getTodos().then((todos) => {
       state.todolist = todos.reverse();
     });
-     model.getCompleted().then((done) => {
-      console.log(done)
+    model.getCompleted().then((done) => {
+      console.log(done);
       state.completedlist = done.reverse();
     });
   };
 
-  const  bootstrap =  () => {
-     init();
+  const bootstrap = () => {
+    init();
     deleteTodo();
     addTodo();
     editTodo();
     completeTodo();
-    undo(),
-    deleteDone();
+    undo(), deleteDone();
     editDone();
   };
 
--- mvc/view.js
+++ mvc/view.js
@@ -23,18 +23,22 @@ const View = (() => {
         </li>
       `;}else{
 				tmp += `
-        <li>
-          <span>${todo.title}</span>
-          <button class="deletebtn" id="${todo.id}">X</button>
-          <div class="edit"> 
-            <i class="fa fa-edit" id="${todo.id}"></i>
-          </div>
-          
-          	<svg  class="finish1" id="${todo.id}" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowForwardIcon" aria-label="fontSize small"><path class="finish2" d="m12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></svg>
+				<li>
+				<div class="text-area">
+					<span>${todo.title}</span>
+				</div>
+				<div class="button-area">
+						<svg class="editbtn" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path id="delete" d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>
+
+						<svg class="deletebtn" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path id="edit" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
+
+						<svg class="finish" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowForwardIcon" aria-label="fontSize small"><path id="done"d="m12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></svg>
+				</div>
+
+
 					
-            
-        </li>
-      `;
+			</li>
+		`;
 			}
 		})
 			return tmp;
@@ -54,14 +58,16 @@ const View = (() => {
       `;}else{
 				tmp += `
         <li>
-        <svg id="${todo.id}" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowBackIcon" aria-label="fontSize small"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path></svg>
+					<div class="text-area">
+          	<span>${todo.title}</span>
+					</div>
+					<div class="button-area">
+         		 <svg class="editbtn" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path id="delete" d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>
 
-          <span>${todo.title}</span>
+							<svg class="deletebtn" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path id="edit" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
 
-					<button class="deletebtn" id="${todo.id}">X</button>
-
-          <i id="${todo.id}" class="fa fa-edit"></i>
-
+							<svg class="finish" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowForwardIcon" aria-label="fontSize small"><path id="done"d="m12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></svg>
+					</div>     
         </li>
       `;
 			}
@@ -80,11 +86,13 @@ const View = (() => {
           	<span>${todo.title}</span>
 					</div>
 					<div class="button-area">
-         		 <svg class="editbtn" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path id="delete" d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>
+         		 <svg class="editbtn" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small">
+							<path id="edit" d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path>
+							</svg>
 
-							<svg class="deletebtn" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path id="edit" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
+							<svg class="deletebtn" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path id="delete" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
 
-							<svg class="finish" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowForwardIcon" aria-label="fontSize small"><path id="done"d="m12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></svg>
+							<svg class="finish" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowForwardIcon" aria-label="fontSize small"><path id="done" d="m12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></svg>
 					</div>
 
 
